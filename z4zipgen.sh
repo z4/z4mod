65176705cf842a07b46f988c62a6d6f8a426c40a
Elia Yehuda <z4ziggy@gmail.com>
2010-10-30 02:32:47 +0200

    added kernel-flash support to update.zip generator

--- z4zipgen.sh
+++ z4zipgen.sh
@@ -11,12 +11,13 @@
 usage() 
 {
 	echo
-	echo "z4zipgen.sh [filesystem] [root] [busybox]"
+	echo "z4zipgen.sh [filesystem] [root] [busybox] [-z zImage]"
 	echo "all parameters are optional:"
 	echo
 	echo "filesystem = convert filesystem to [ext2/ext3/ext4/jfs/rfs/auto]"
 	echo "root       = install root"
 	echo "busybox    = install busybox"
+	echo "-z zImage  = flash zImage to device"
 	echo
 	exit 1
 }
@@ -33,24 +34,32 @@ set_data_filesystem()
 
 
 [ $# == 0 ] && usage
-[ $# -gt 3 ] && usage
 
 srcdir=`dirname $0`
-wrkdir=`pwd`/tmp/z4mod-$$-$RANDOM
+srcdir=`realpath $srcdir`
+wrkdir=`pwd`/z4mod-$$-$RANDOM.tmp
 script=${wrkdir}/META-INF/com/google/android/updater-script
 mkdir -p ${wrkdir}/{sbin,system/{app,xbin}}
 cp -r ${srcdir}/updates/META-INF ${wrkdir}/
 
 filename=z4mod
-z4install="false"
 
 while [ "$*" ]; do
-	filename="${filename}.$1"
 	if [ "$1" == "root" -o "$1" == "busybox" ]; then
 		cp -r ${srcdir}/updates/$1/* ${wrkdir}/
 		z4install="true"
+		filename="${filename}.$1"
+	elif [ "$1" == "-z" ]; then
+		shift
+		zImage=`realpath $1`
+		if [ ! -f $zImage ]; then
+			echo "zImage not found: $zImage"
+			exit 1
+		fi
+		filename="${filename}.zImage"
 	else
 		filesystem="$1"
+		filename="${filename}.$1"
 	fi
 	shift
 done
@@ -90,17 +99,29 @@ case "${filesystem}" in
 		;;
 esac
 
-if [ ${z4install} == "false" ]; then
+if [ -z ${z4install} ]; then
 	# remove the install section from updater-script
 	sed -i '/# START: Install/,/# END: Install/d' "${script}"
 fi
+if [ -z ${zImage} ]; then
+	# remove the kernel-flash section from updater-script
+	sed -i '/# START: Kernel/,/# END: Kernel/d' "${script}"
+else
+	cp ${srcdir}/updates/redbend_ua ${wrkdir}/redbend_ua
+	cp $zImage $wrkdir/zImage
+	zImagefiles="zImage redbend_ua"
+fi
 
 # set version in script
-version=$(< z4version)
+version=`cat ${srcdir}/z4version`
 sed -i 's/Version .*/Version '$version'\");/g' ${script}
 # create the update.zip file
 curdir=`pwd`
-(cd ${wrkdir}; zip -r $curdir/${filename}.update.zip META-INF/ sbin/ system/)
+(cd ${wrkdir}; zip -r $curdir/${filename}.update.zip META-INF/ sbin/ system/ $zImagefiles)
 # cleanup
 rm -rf ${wrkdir}
 
+echo
+echo File is ready: 
+ls -lh $filename.update.zip
+echo
