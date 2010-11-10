181cf82434e0b650a858c471c5ea798d48d62110
Elia Yehuda <z4ziggy@gmail.com>
2010-11-10 18:22:49 +0200

    fix su

--- initramfs/root/z4root.sh
+++ initramfs/root/z4root.sh
@@ -5,5 +5,6 @@ if [ ! -f /system/app/Superuser.apk ]; then
 	busybox mount -o remount,rw /system
 	busybox cp /res/Superuser.apk /system/app/Superuser.apk
 	busybox cp -a /sbin/su /system/xbin/su
+	chmod 6755 /system/xbin/su
 	busybox mount -o remount,ro /system
 fi
